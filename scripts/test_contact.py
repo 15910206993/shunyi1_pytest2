398e207f410b72f953bf32f98ed3a309cee9d409
15910206993 <15910206993@163.com>
2018-11-12 16:24:18 +0800

    74110

---
+++ scripts/test_contact.py
@@ -0,0 +1,17 @@
+# import time
+# import pytest
+#
+# from base.base_driver import init_driver
+# from page.page import Page
+# from base.base_analyze import analyze_file
+
+
+class TestContact:
+    def test_1(self):
+        assert 1
+    def test_2(self):
+        assert 1
+    def test_3(self):
+        assert 0
+
+
